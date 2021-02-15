3d535b046a50331f1f23fc96497174e2493f81a0
Afanasegor <afanasegor3@mail.ru>
2021-02-15 15:12:04 +0300

    First commit

---
+++ InvokerSkills/AllCombos.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvokerSkills
+{
+    public static class AllCombos
+    {
+        public static string WhatSkill(string combo)
+        {
+            string skillName = null;
+
+            if (combo == "qqq")
+            {
+                skillName = "ColdSnap";
+            }
+            else if (combo == "qqw" || combo == "qwq" || combo == "wqq")
+            {
+                skillName = "GhostWalk";
+            }
+            else if (combo == "qqe" || combo == "qeq" || combo == "eqq")
+            {
+                skillName = "IceWall";
+            }
+            else if (combo == "www")
+            {
+                skillName = "EMP";
+            }
+            else if (combo == "wwq" || combo == "wqw" || combo == "qww")
+            {
+                skillName = "Tornado";
+            }
+            else if (combo == "wwe" || combo == "wew" || combo == "eww")
+            {
+                skillName = "Alacrity";
+            }
+            else if (combo == "eee")
+            {
+                skillName = "SunStrike";
+            }
+            else if (combo == "eeq" || combo == "eqe" || combo == "qee")
+            {
+                skillName = "ForgeSpirit";
+            }
+            else if (combo == "eew" || combo == "ewe" || combo == "wee")
+            {
+                skillName = "Meteorite";
+            }
+            else if (combo == "qwe" || combo == "qew" || combo == "weq" || combo == "wqe" || combo == "eqw" || combo == "ewq")
+            {
+                skillName = "Blast";
+            }
+            else if (combo == "stop" || combo == "STOP" || combo == "Stop")
+            {
+                skillName = "STOP";
+            }
+            else if (combo == "R" || combo == "r" || combo == "Refresher" || combo == "refresher" || combo == "ref")
+            {
+                skillName = "Refresher";
+            }
+            else
+            {
+                skillName = "Wrong Combo!";
+            }
+
+            return skillName;
+        }
+    }
+}
---
+++ InvokerSkills/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace InvokerSkills
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Давай кастуй!");
+            Console.WriteLine();
+            string[] allSkills = { "ColdSnap", "GhostWalk", "IceWall", "EMP", "Tornado", "Alacrity", "SunStrike", "ForgeSpirit", "Meteorite", "Blast" };
+            string[] skillCooldown = allSkills;
+            string cast = null;            
+            Stopwatch sw = new Stopwatch();
+            
+            sw.Start();
+            while (skillCooldown.Length > 0 && cast != "STOP")
+            {
+                string combo = Console.ReadLine();
+                cast = AllCombos.WhatSkill(combo);
+                DeleteSkill(ref skillCooldown, cast, allSkills);
+                Result(skillCooldown, cast);
+            }
+            sw.Stop();            
+            Console.WriteLine();
+            Console.WriteLine($"Время потрачено: {sw.Elapsed}");
+        }
+
+        static void DeleteSkill(ref string[] m, string cast, string[] allSkills) //то, что ниже закоментировано, это на случай метода без использования "ref"
+        {
+            if (cast == "Refresher")
+            {
+                //return allSkills;
+                m = allSkills;
+            }
+            else
+            {
+                string[] m2 = new string[m.Length - 1];
+                bool check = false;
+                int k = 0;
+                for (int i = 0; i < m.Length; i++)
+                {
+                    if (cast == m[i])
+                    {
+                        check = true;
+                    }
+                }
+                if (check == true)
+                {
+                    for (int i = 0; i < m.Length; i++)
+                    {
+                        if (cast == m[i])
+                        {
+                            continue;
+                        }
+                        m2[k] = m[i];
+                        k++;
+                    }
+                    //return m2;
+                    m = m2;
+                }
+                //else
+                //{
+                //    return m;
+                //}
+            }
+        }
+
+        static void Result(string[] skillCooldown, string cast)
+        {
+            if (cast == "STOP")
+            {
+                Console.WriteLine("Ручная остановка программы");
+            }
+            else if (skillCooldown.Length > 0)
+            {
+                Console.WriteLine(skillCooldown.Length);
+                Console.Write("Скиллы не в кулдауне: ");
+                for (int j = 0; j < skillCooldown.Length; j++)
+                {
+                    Console.Write(skillCooldown[j] + " ");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Вот ты и потратил все скиллы...");
+            }
+            
+        }
+    }
+}
